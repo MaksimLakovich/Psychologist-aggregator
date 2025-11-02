08fa24911cf79e8c471f32f2fb4a89ca83036fef
Maksim Lakovich <maks.lakovich@gmail.com>
2025-11-02 11:43:41 +0200

    1) В приложении Users добавлена модель PsychologistProfile. 2) Добавлен модуль constants.py с переменными и справочниками моделей. 3) Обновлен README.md

---
+++ users/constants.py
@@ -0,0 +1,20 @@
+# Справочники для моделей в users/models.py
+GENDER_CHOICES = [
+    ("male", "мужской"),
+    ("female", "женский"),
+]
+
+LANGUAGE_CHOICES = [
+    ("english", "английский"),
+    ("russian", "русский"),
+]
+
+THERAPY_FORMAT_CHOICES = [
+    ("online", "удаленно"),
+    ("offline", "встреча"),
+]
+
+WORK_STATUS_CHOICES = [
+    ("working", "работает"),
+    ("not_working", "не работает"),
+]
