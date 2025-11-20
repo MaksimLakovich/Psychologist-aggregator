e60489b9c6f98edaec3bf02f487812a0daa3bfe9
Maksim Lakovich <maks.lakovich@gmail.com>
2025-11-20 20:27:40 +0200

    1) Изменен миксин CreatorMixin() в users/mixins/creator_mixin.py. 2) Обновлен README.md

--- users/mixins/creator_mixin.py
+++ users/mixins/creator_mixin.py
@@ -2,16 +2,10 @@ from rest_framework import serializers
 
 
 class CreatorMixin(serializers.ModelSerializer):
-    """Миксин, который автоматически заполняет поле creator текущим пользователем при создании или обновлении."""
+    """Миксин, который автоматически заполняет поле creator текущим пользователем при создании объекта."""
 
     def create(self, validated_data):
         request = self.context.get("request")
         if request and hasattr(request, "user"):
             validated_data["creator"] = request.user
         return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        request = self.context.get("request")
-        if request and hasattr(request, "user"):
-            validated_data["creator"] = request.user
-        return super().update(instance, validated_data)
