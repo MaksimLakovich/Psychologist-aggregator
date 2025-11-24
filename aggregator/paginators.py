5a19eb757c8797aa26df4768f7ef16db79ff66e8
Maksim Lakovich <maks.lakovich@gmail.com>
2025-11-24 14:33:47 +0200

    Инициализировано приложение Aggregator

---
+++ aggregator/paginators.py
@@ -0,0 +1,9 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class PsychologistCatalogPagination(PageNumberPagination):
+    """Класс-пагинатор для страницы с *Публичным каталогом психологов*."""
+
+    page_size = 24  # Количество элементов на странице
+    page_size_query_param = "catalog_page_size"  # Название параметра запроса для указания кол-ва элементов на странице
+    max_page_size = 100  # Максимальное количество элементов на странице
