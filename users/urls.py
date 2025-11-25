9ff50da543cdd2652358469cafa22fafbc886c08
Maksim Lakovich <maks.lakovich@gmail.com>
2025-11-25 21:01:49 +0200

    Доработал сериализатор в приложении Users по части работы с timezone (устранение ошибки)

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     # API (DRF)
-    path("", include("users._api.urls", namespace="api")),
+    path("api/", include("users._api.urls", namespace="api")),
 
     # WEB (обычные Django views + шаблоны)
     # path("", include("users._web.urls", namespace="web")),
