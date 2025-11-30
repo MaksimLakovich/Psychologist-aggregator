9a9aaf07dc9c831aef3a1149478a1a628b1e6f83
Maksim Lakovich <maks.lakovich@gmail.com>
2025-12-01 00:41:27 +0200

    1. Создал LOGIN. 2. Доработал README.md

--- users/_web/urls.py
+++ users/_web/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from users._web.views.auth_view import AppUserLoginView
+from users._web.views.auth_view import LoginPageView
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("login/", AppUserLoginView.as_view(), name="login_page"),
+    path("login/", LoginPageView.as_view(), name="login_page"),
 ]
