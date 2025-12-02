a55c3f72b1ad6caec319e3408a030f756d3ec5ff
Maksim Lakovich <maks.lakovich@gmail.com>
2025-12-02 21:23:41 +0200

    Общие вопросы (первая домашняя страница для клиента)

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from core.apps import CoreConfig
-from core.views.home_view import HomePageView
+from core.views.home_client_general_questions import \
+    ClientGeneralQuestionsPageView
 from core.views.start_view import StartPageView
 
 app_name = CoreConfig.name
 
 urlpatterns = [
     path("", StartPageView.as_view(), name="start_page"),
-    path("home/", HomePageView.as_view(), name="home_page"),
+    path("general-questions/", ClientGeneralQuestionsPageView.as_view(), name="client_general_questions_page"),
 ]
