d2c1ed3edf83a44e0e88219140e7c5ef82b90e16
Maksim Lakovich <maks.lakovich@gmail.com>
2026-02-13 23:06:23 +0200

    Добавленый авы и подсчет верифицированных психологов в БД для страницы "ВХОД" (login_page.html)

--- static/js/modules/login_page_stats.js
+++ static/js/modules/login_page_stats.js
@@ -4,5 +4,5 @@ const statsEl = document.getElementById("verified-psychologists-text");
 if (statsEl) {
   const count = Number(statsEl.dataset.count || 0);
   const word = pluralizeRu(count, "специалист", "специалиста", "специалистов");
-  statsEl.innerHTML = `В команде <span class="font-bold">${count}</span> ${word}, готовых помочь в любое время суток`;
+  statsEl.innerHTML = `В команде <span class="font-bold">${count}</span> ${word}, готовых помочь прямо сейчас!`;
 }
