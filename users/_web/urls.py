1908dd4d54b95acd1edf485d9d58dc31a3aec277
Maksim Lakovich <maks.lakovich@gmail.com>
2026-02-15 11:53:40 +0200

    Подготовка для реализации ВОССТАНОВЛЕНИЯ пароля

--- users/_web/urls.py
+++ users/_web/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from users._web.views.auth_view import (LoginPageView, RegisterPageView,
                                         VerifyEmailView)
+from users._web.views.reset_password_view import (PasswordResetConfirmPageView,
+                                                  PasswordResetRequestPageView)
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
@@ -13,6 +15,8 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", RegisterPageView.as_view(), name="register-page"),
     path("verify-email/", VerifyEmailView.as_view(), name="verify-email"),
+    path("password-reset/", PasswordResetRequestPageView.as_view(), name="password-reset"),
+    path("password-reset-confirm/", PasswordResetConfirmPageView.as_view(), name="password-reset-confirm"),
 
     # Работа с аккаунтом / Работа с профилем
 ]
